2cf8b937e10e103def6dd091322e512be3bda024
Danial <danialkassymgmail.com>
2025-03-14 21:47:41 +0500

    Implemented Yamlhandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v3"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -39,6 +41,24 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var paths []pathToURL
+
+	err := yaml.Unmarshal(yml, &paths)
+	if err != nil {
+		return nil, err
+	}
+
+	url := make(map[string]string)
+	
+	for _, add := range paths {
+		url[add.Path] = add.URL
+	}
+
+	return MapHandler(url, fallback), nil
+
+}
+
+type pathToURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
